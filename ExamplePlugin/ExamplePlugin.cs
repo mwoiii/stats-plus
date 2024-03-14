56fd79449949cea84abcb81d5ed6181be99f0911
mwoiii <147194899+mwoiii@users.noreply.github.com>
2024-03-14 15:59:03 +0000

    Added idle tracker
    Time that a player is standing still for is now recorded by using the same method as bungus activation
    
    Also slightly modified implementation of the shrine tracker, and added a method to reset data dictionaries

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Networking;
 using System.Collections.Generic;
+using R2API.Utils;
+using System;
 
 namespace ExamplePlugin
 {
@@ -47,6 +49,8 @@ namespace ExamplePlugin
         private IDictionary<NetworkUser, int> shrinePurchases = new Dictionary<NetworkUser, int>();  // Dictionary for recording how many times each player has used a shrine of chance
         private IDictionary<NetworkUser, int> shrineWins = new Dictionary<NetworkUser, int>();  // Dictionary for recording how many times each player has won a shrine of chance
         private IDictionary<NetworkUser, int> shrineLoses = new Dictionary<NetworkUser, int>();  // Dictionary for recording how many times each player has lost a shrine of chance
+
+        private IDictionary<NetworkUser, uint> timeStill = new Dictionary<NetworkUser, uint>();  // Dictionary for recording how long each player has been standing still
 
         // We need our item definition to persist through our functions, and therefore make it a class field.
         private static ItemDef myItemDef;
@@ -141,36 +145,16 @@ namespace ExamplePlugin
 
         private void Enable()  // When this method is called, enabling all mod features
         {
-            RoR2.ShrineChanceBehavior.onShrineChancePurchaseGlobal += ShrineTrack;
+            ShrineChanceBehavior.onShrineChancePurchaseGlobal += ShrineTrack;
+            Run.onRunStartGlobal += ResetData;
         }
 
 
         private void Disable() // When this method is called, disabling all mod features
         {
-            RoR2.ShrineChanceBehavior.onShrineChancePurchaseGlobal -= ShrineTrack;
+            ShrineChanceBehavior.onShrineChancePurchaseGlobal -= ShrineTrack;
         }
 
-        /*
-        // Tracks all the times shrines are hit. This method is only ever called on the host side
-        private void ShrineTrack(On.RoR2.ShrineChanceBehavior.orig_AddShrineStack orig, global::RoR2.ShrineChanceBehavior self, global::RoR2.Interactor activator)
-        {
-            var networkPlayer = activator.GetComponent<CharacterBody>().master.playerCharacterMasterController.networkUser;  // Getting the networkUser (unique identification in multiplayer), calling it networkPlayer
-            if (!shrinePurchases.ContainsKey(networkPlayer))  // If networkPlayer isn't in the shrinePurchases dictionary, adding them. Otherwise, incrementing counter by 1
-            {
-                shrinePurchases.Add(networkPlayer, 1);
-            }
-            else
-            {
-                shrinePurchases[networkPlayer]++;
-            }
-
-            Log.Info(shrinePurchases[networkPlayer]); // Just for demonstration purposes, you can see in the log that the counter goes up seperately for each player and is maintained across stages
-            Log.Info(networkPlayer);
-
-            orig(self, activator);  // Calling original method
-
-        }
-        */
 
         // Tracks all the times shrines are hit. This method is only ever called on the host side
         private void ShrineTrack(bool failed, Interactor activator)
@@ -196,12 +180,82 @@ namespace ExamplePlugin
                 shrineWins[networkPlayer]++;
             }
 
-            Log.Info(shrinePurchases[networkPlayer]); // Just for demonstration purposes, you can see in the log that the counter goes up seperately for each player and is maintained across stages
-            Log.Info(shrineWins[networkPlayer]);
-            Log.Info(shrineLoses[networkPlayer]);
+            //Log.Info(shrinePurchases[networkPlayer]); // Just for demonstration purposes, you can see in the log that the counter goes up seperately for each player and is maintained across stages
+            //Log.Info(shrineWins[networkPlayer]);
+            //Log.Info(shrineLoses[networkPlayer]);
 
         }
 
+        // Counting how long a player has stopped moving for
+        public void FixedUpdate()
+        {
+            if (NetworkServer.active)
+            {
+                if (PlayerCharacterMasterController.instances.Count > 0)  // Checking if the player is in a run by checking for existence of PlayerCharacterMasterController, which is created at the start of a run
+                {
+                    foreach (PlayerCharacterMasterController player in PlayerCharacterMasterController.instances)
+                    {
+                        try
+                        {
+                            var isStill = player.master.GetBody().GetNotMoving();
+                            if (isStill)
+                            {
+                                var networkPlayer = player.networkUser;
+                                if (!timeStill.ContainsKey(networkPlayer))
+                                {
+                                    timeStill.Add(networkPlayer, 1);
+                                }
+                                else
+                                {
+                                    timeStill[networkPlayer]++;
+                                }
+                                // Log.Info(timeStill[networkPlayer] * Time.fixedDeltaTime);  // FixedUpdate is called a different amount of times depending on the framerate. Time.fixedDeltaTime is the frequency that it is called
+                            }
+                        }
+                        catch (NullReferenceException)
+                        {
+                            // Player may be dead, or not properly spawned yet
+                        }
+                    }
+
+                }
+            }
+        }
+
+        // Emptying all the data dictionaries. Called at the start of a run
+        private void ResetData(Run run)
+        {
+            Log.Info("New run, resetting data dicts");
+            shrinePurchases = new Dictionary<NetworkUser, int>();  // Dictionary for recording how many times each player has used a shrine of chance
+            shrineWins = new Dictionary<NetworkUser, int>();  // Dictionary for recording how many times each player has won a shrine of chance
+            shrineLoses = new Dictionary<NetworkUser, int>();  // Dictionary for recording how many times each player has lost a shrine of chance
+
+            timeStill = new Dictionary<NetworkUser, uint>();  // Dictionary for recording how long each player has been standing still
+        }
+
+
+        // Old implementation of the shrine hit method using hooking
+        /*
+        // Tracks all the times shrines are hit. This method is only ever called on the host side
+        private void ShrineTrack(On.RoR2.ShrineChanceBehavior.orig_AddShrineStack orig, global::RoR2.ShrineChanceBehavior self, Interactor activator)
+        {
+            var networkPlayer = activator.GetComponent<CharacterBody>().master.playerCharacterMasterController.networkUser;  // Getting the networkUser (unique identification in multiplayer), calling it networkPlayer
+            if (!shrinePurchases.ContainsKey(networkPlayer))  // If networkPlayer isn't in the shrinePurchases dictionary, adding them. Otherwise, incrementing counter by 1
+            {
+                shrinePurchases.Add(networkPlayer, 1);
+            }
+            else
+            {
+                shrinePurchases[networkPlayer]++;
+            }
+
+            Log.Info(shrinePurchases[networkPlayer]); // Just for demonstration purposes, you can see in the log that the counter goes up seperately for each player and is maintained across stages
+            Log.Info(networkPlayer);
+
+            orig(self, activator);  // Calling original method
+
+        }
+        */
 
         // Old test code for the spawning a behemoth on every bullet spawn
         /*
@@ -219,27 +273,27 @@ namespace ExamplePlugin
         }
         */
 
-
-        // Old test code for spawning a behemoth and killing the player on f2 press
+       // Old test code for spawning a behemoth and killing the player on f2 press
         /*
         // The Update() method is run on every frame of the game.
         private void Update()
         {
+
             // This if statement checks if the player has currently pressed F2.
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                // Get the player body to use a position:
                 var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-
-                // And then drop our defined item in front of the player.
-
                 Log.Info($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
                 Log.Info("Changes were applied");
 
                 PlayerCharacterMasterController.instances[0].master.TrueKill();
                 PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(RoR2Content.Items.Behemoth.itemIndex), transform.position, transform.forward * 20f);
             }
+            
         }
         */
+        
     }
+    
+
 }
