e3d9869a908a340e074a4250aa6a21c84c155f42
mwoiii <147194899+mwoiii@users.noreply.github.com>
2025-08-11 21:10:06 +0100

    allypoints -> allies
    instead of arbitrary points, just tries to count allies

---
+++ StatsMod/CustomStats/Stats/Allies.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RoR2;
+
+namespace StatsMod.CustomStats {
+    internal class Allies : Stat {
+        private static Dictionary<PlayerCharacterMasterController, uint> alliesDict = [];  // How many times each player has used a shrine of order
+        new public static void Init() {
+            SceneExitController.onBeginExit += AllyCountTrackLeaveStage;
+            Run.onServerGameOver += AllyCountTrackRunOver;
+
+            Tracker.statsTable.Add("allies", alliesDict);
+        }
+
+        private static void AllyCountTrackRunOver(Run run, GameEndingDef gameEndingDef) {
+            CountAllies();
+        }
+
+        private static void AllyCountTrackLeaveStage(SceneExitController sceneExitController) {
+            CountAllies();
+        }
+
+        private static void CountAllies() {
+            // will not actually count all allies
+            // just most of the relevant vanilla ones
+            foreach (var player in PlayerCharacterMasterController.instances) {
+                var inventory = player.master.inventory;
+                CharacterMaster master = player.master;
+                int droneCount = CharacterMaster.readOnlyInstancesList.Where(u => (u.name.Contains("Drone") || u.name.Contains("Turret")) && u.minionOwnership.ownerMaster == master).Count();
+                int sum = (
+                    master.GetDeployableSameSlotLimit(DeployableSlot.BeetleGuardAlly) +
+                    master.GetDeployableSameSlotLimit(DeployableSlot.ParentAlly) +
+                    master.GetDeployableSameSlotLimit(DeployableSlot.VoidMegaCrabItem) +
+                    (inventory.GetItemCount(RoR2Content.Items.RoboBallBuddy) > 0 ? 2 : 0) +
+                    droneCount
+                    );
+                if (alliesDict.ContainsKey(player)) { alliesDict[player] = (uint)sum; } else { alliesDict.Add(player, (uint)sum); }
+            }
+        }
+    }
+}
--- StatsMod/PlayerStatsDatabase.cs
+++ StatsMod/PlayerStatsDatabase.cs
@@ -1,15 +1,13 @@
-﻿using RoR2;
-using RoR2.Stats;
+﻿using System;
 using System.Collections.Generic;
-using System;
-using System.Text;
 using System.Linq;
+using System.Text;
+using RoR2;
+using RoR2.Stats;
 using StatsMod.CustomStats;
 
-namespace StatsMod
-{
-    public class PlayerStatsDatabase
-    {
+namespace StatsMod {
+    public class PlayerStatsDatabase {
         private readonly PlayerCharacterMasterController player;
         private readonly int playerIndex;
         private readonly string playerName;
@@ -20,19 +18,17 @@ namespace StatsMod
         // "critHeal", "multiKillCount", "level", "acceleration", "experience", "isPlayerControlled", "isSprinting", "outOfDanger", "jumpPower", "maxJumpHeight", "shouldAim", "bestFitRadius", "spreadBloomAngle", "corePosition", "footPosition", "radius", "aimOrigin", "isBoss"
         public static readonly string[] charBodyStats = ["maxHealth", "regen", "maxShield", "moveSpeed", "maxJumpCount", "damage", "attackSpeed", "crit", "armor", "isElite"];
         // "totalStagesCompleted", "maxGoldCollected", "highestLevel", "totalGamesPlayed", "highestItemsCollected", "highestStagesCompleted", "highestPurchases", "highestGoldPurchases", "highestBloodPurchases", "highestLunarPurchases", "highestTier1Purchases", "highestTier2Purchases", "highestTier3Purchases", "suicideHermitCrabsAchievementProgress", "firstTeleporterCompleted"
-        public static readonly string[] statSheetStats = ["totalTimeAlive", "totalKills", "totalMinionKills", "totalDeaths", "totalDamageDealt", "totalMinionDamageDealt", "totalDamageTaken", "totalHealthHealed", "highestDamageDealt",  "goldCollected", "totalDistanceTraveled", "totalItemsCollected", "totalPurchases", "totalGoldPurchases", "totalBloodPurchases", "totalLunarPurchases", "totalTier1Purchases", "totalTier2Purchases", "totalTier3Purchases", "totalDronesPurchased", "totalTurretsPurchased", "totalGreenSoupsPurchased", "totalRedSoupsPurchased"];
-        public static readonly string[] customStats = ["shrinePurchases", "shrineWins", "orderHits", "timeStill", "timeStillUnsafe", "timeLowHealth", "fallDamage", "coinsSpent", "avenges", "timesLastStanding", "itemLead", "nonScrapPrinted", "likelyDonations", "allyPoints"]; // "chainableProcs" (disabled)
+        public static readonly string[] statSheetStats = ["totalTimeAlive", "totalKills", "totalMinionKills", "totalDeaths", "totalDamageDealt", "totalMinionDamageDealt", "totalDamageTaken", "totalHealthHealed", "highestDamageDealt", "goldCollected", "totalDistanceTraveled", "totalItemsCollected", "totalPurchases", "totalGoldPurchases", "totalBloodPurchases", "totalLunarPurchases", "totalTier1Purchases", "totalTier2Purchases", "totalTier3Purchases", "totalDronesPurchased", "totalTurretsPurchased", "totalGreenSoupsPurchased", "totalRedSoupsPurchased"];
+        public static readonly string[] customStats = ["shrinePurchases", "shrineWins", "orderHits", "timeStill", "timeStillUnsafe", "timeLowHealth", "fallDamage", "coinsSpent", "avenges", "timesLastStanding", "itemLead", "nonScrapPrinted", "likelyDonations", "allies"]; // "chainableProcs" (disabled)
 
         public static IEnumerable<string> allStats = charBodyStats.Union(statSheetStats).Union(customStats);
 
-        public PlayerStatsDatabase(PlayerCharacterMasterController instance)
-        {
+        public PlayerStatsDatabase(PlayerCharacterMasterController instance) {
             player = instance;
             playerIndex = PlayerCharacterMasterController.instances.IndexOf(player);
-            
+
             playerName = player.networkUser.userName;
-            if (playerName.Length == 0)
-            {
+            if (playerName.Length == 0) {
                 Log.Warning($"No name found for player with index {playerIndex}, will use index to reference instead. Can by caused by singleplayer server testing.");
                 playerName = $"Player {playerIndex}";
             }
@@ -41,22 +37,17 @@ namespace StatsMod
             foreach (string statName in allStats) { Database.Add(statName, []); }
         }
 
-        public float TakeRecord()
-        {
+        public float TakeRecord() {
             float timestamp = Run.instance.GetRunStopwatch();
             Database["timestamps"].Add(timestamp);
 
             // Getting charBody stats
             CharacterBody CachedCharacterBody = player.master.GetBody();  // Getting reference to specific player
-            if (CachedCharacterBody == null)
-            {
+            if (CachedCharacterBody == null) {
                 Log.Warning($"No body found for {playerName} at {timestamp}. Duplicate entries added to database");
                 foreach (string i in charBodyStats) { Database[i].Add(Database[i].Last()); }
-            }
-            else
-            {
-                foreach(string i in charBodyStats)
-                {
+            } else {
+                foreach (string i in charBodyStats) {
                     object stat = typeof(CharacterBody).GetProperty(i).GetValue(CachedCharacterBody);
                     Database[i].Add(stat);
                 }
@@ -65,15 +56,11 @@ namespace StatsMod
             // Getting statSheet stats
             RunReport runReport = RunReport.Generate(Run.instance, GameEndingCatalog.GetGameEndingDef((GameEndingIndex)2));
             RunReport.PlayerInfo playerInfo = runReport.GetPlayerInfo(playerIndex);
-            if (playerInfo == null)
-            {
+            if (playerInfo == null) {
                 Log.Warning($"No stat sheet found for {playerName} at {timestamp}. Duplicate entries added to database");
                 foreach (string i in statSheetStats) { Database[i].Add(Database[i].Last()); }
-            }
-            else
-            {
-                foreach (string i in statSheetStats)
-                {
+            } else {
+                foreach (string i in statSheetStats) {
                     StatDef statDef = (StatDef)typeof(StatDef).GetField(i).GetValue(null);
                     object stat = playerInfo.statSheet.GetStatDisplayValue(statDef);
                     Database[i].Add(stat);
@@ -81,15 +68,11 @@ namespace StatsMod
             }
 
             // Getting custom stats
-            foreach (string i in customStats)
-            {
-                try
-                {
+            foreach (string i in customStats) {
+                try {
                     var stat = Tracker.GetStat(player, i);
                     Database[i].Add(stat);
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Database[i].Add(Database[i].Last());
                     Log.Warning($"Failed customStat {i} for {playerName} at {timestamp}, duplicate entry added. \n {e.Message}");
                 }
@@ -98,8 +81,7 @@ namespace StatsMod
             return timestamp;
         }
 
-        public List<object> GetStatSeries(string name)
-        {
+        public List<object> GetStatSeries(string name) {
             return Database[name];
         }
 
@@ -107,33 +89,27 @@ namespace StatsMod
         {
             List<object> series = GetStatSeries(name);
             StringBuilder a = new();
-            foreach (object entry in series)
-            {
-                if (!rVector) { a.Append($"{entry}, "); }
-                else { a.Append($"{Numberise(entry)}, "); }
+            foreach (object entry in series) {
+                if (!rVector) { a.Append($"{entry}, "); } else { a.Append($"{Numberise(entry)}, "); }
             }
             string b = a.ToString().Substring(0, Math.Max(0, a.Length - 2));
 
-            if (!rVector) { return $"{name}: {b}"; }
-            else { return $"{name} <- c({b})"; }
-            
+            if (!rVector) { return $"{name}: {b}"; } else { return $"{name} <- c({b})"; }
+
         }
 
-        public Dictionary<string, object> GetRecord(int index)
-        {
+        public Dictionary<string, object> GetRecord(int index) {
             if (index < 0) { index = Database["maxHealth"].Count + index; }
             Dictionary<string, object> Record = [];
             foreach (string statName in allStats) { Record.Add(statName, Database[statName][index]); }
             return Record;
         }
 
-        public Dictionary<string, object> GetRecord(float time)
-        {
+        public Dictionary<string, object> GetRecord(float time) {
             List<object> timestamps = Database["timestamps"];
             int index;
 
-            try { index = timestamps.IndexOf(time); }
-            catch { index = timestamps.IndexOf(timestamps.OrderBy(x => Math.Abs(float.Parse(x.ToString()) - time)).First()); }
+            try { index = timestamps.IndexOf(time); } catch { index = timestamps.IndexOf(timestamps.OrderBy(x => Math.Abs(float.Parse(x.ToString()) - time)).First()); }
 
             return GetRecord(index);
         }
@@ -141,21 +117,18 @@ namespace StatsMod
         public static object Numberise(object value) // Takes non numerical object types that can represent numbers and turns them into numerical types to be interpreted.
         {
             if (value.GetType() == typeof(bool)) { return (bool)value ? 1 : 0; } // bools turned to 0s or 1s
-            
+
             string valueString = value.ToString();
-            
+
             if (valueString.Contains(',')) // numbers represented with commmas have commas removed
             {
-                try { return Int64.Parse(valueString, System.Globalization.NumberStyles.AllowThousands); }
-                catch ( OverflowException ) {  return -1; }  // Safeguarding, to prevent data from not being recorded at all
-            }
-            else if (valueString.Contains(':')) // minute:second representation turned to just seconds
-            {
+                try { return Int64.Parse(valueString, System.Globalization.NumberStyles.AllowThousands); } catch (OverflowException) { return -1; }  // Safeguarding, to prevent data from not being recorded at all
+            } else if (valueString.Contains(':')) // minute:second representation turned to just seconds
+              {
                 string[] time = valueString.Split(':');
                 return int.Parse(time[0]) * 60 + int.Parse(time[1]);
-            }
-            else if (valueString.Contains("marathons")) // "marathons" representation have the "marathons" part removed
-            {
+            } else if (valueString.Contains("marathons")) // "marathons" representation have the "marathons" part removed
+              {
                 return float.Parse(valueString.Substring(0, valueString.Length - 10));
             }
             return value;
